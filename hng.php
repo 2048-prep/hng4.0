dd424697d67f69f9c1aef355b2ac108f1a10cb1c
2048-prep <solahudeen15@gmail.com>
2018-04-23 15:17:17 +0100

    Add files via upload
    The HTML page that displays current time

---
+++ hng.php
@@ -0,0 +1,102 @@
+<html>
+<head>
+<style>
+body {
+position: relative;
+width: 606px;
+height: 412px;
+background: #ffffff;}
+
+h2 {
+position: absolute;
+width: 352px;
+height: 34px;
+left: 127px;
+top: 29px;
+background: #f2c94c;
+font-family: roboto;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 32px;
+}
+div#aqua {
+position: absolute;
+width: 606px;
+height: 305px;
+left: 0px;
+top: 112px;
+background: #56ccf2;
+}
+p#one {
+position: absolute;
+width: 126px;
+height: 47px;
+left: 200px;
+top: 62px;
+
+font-family: roboto;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 45px;
+}
+p#two {
+position: absolute;
+width: 93px;
+height: 19px;
+left: 69px;
+top: 216px;
+
+padding: 5px;
+font-family: roboto;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 17px;
+background: #C4C4C4;
+}
+
+p#three {
+position: absolute;
+width: 222px;
+height: 28px;
+left: 210px;
+top: 255px;
+padding-left:20px;
+font-family: roboto;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 17px;
+background: #C4C4C4;
+}
+p#four {
+position: absolute;
+width: 91px;
+height: 29px;
+left: 482px;
+top: 212px;
+
+font-family: roboto;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 17px;
+background: #C4C4C4;
+}
+</style>
+</head>
+
+<body>
+<center> <h2> THE TIME IS : </h2> </center>
+<div id = "aqua">
+<p id = "one"> <?php echo date("h:i:sa");?> </p>
+
+<p id = "two"> ABOUT US </p>
+<p id = "three"> HNG INTERNSHIP 4.0 </p>
+<p id = "four">ADDRESS</p>
+
+</div>
+</body>
+</html>
